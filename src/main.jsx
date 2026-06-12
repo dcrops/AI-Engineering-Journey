ed9e3f2d4f3bc9cedbf9ff99cdf602323f8aab38
Dave C. <dave_cropper2003@yahoo.co.uk>
2026-06-12 10:43:49 +1000

    Add Google Analytics tracking to Journey site

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
+import ReactGA from "react-ga4";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+ReactGA.initialize("G-GP9R59R50G");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname + window.location.search,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
